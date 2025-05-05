436063b8d47a7067af4c3f322597f0acac28d53e
Lorenz Wrobel <43410952+DasBabyPixel@users.noreply.github.com>
2025-05-05 19:08:07 +0200

    signing

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,14 +12,14 @@ repositories {
     mavenCentral()
 }
 
-if (hasProperty("SonatypeUsername")) {
+if (System.getenv("SONATYPE_USERNAME") != null) {
     nexusPublishing {
         repositories {
             sonatype {
                 nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
                 snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
-                username.set(System.getenv("SonatypeUsername"))
-                password.set(System.getenv("SonatypePassword"))
+                username.set(System.getenv("SONATYPE_USERNAME"))
+                password.set(System.getenv("SONATYPE_PASSWORD"))
             }
         }
     }
