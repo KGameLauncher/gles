57919265c4e1afd97d53080687010f9c245e5dbd
Lorenz Wrobel <43410952+DasBabyPixel@users.noreply.github.com>
2025-05-05 18:27:29 +0200

    actions first try

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,6 +17,8 @@ nexusPublishing {
         sonatype {
             nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
             snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+            username.set(property("SonatypeUsername")!!.toString())
+            password.set(property("SonatypePassword")!!.toString())
         }
     }
 }
